fde1bac4446ed9c614c7e788e620667eb0edffab
Simon Morris <simonjmorris213@gmail.com>
2022-08-02 20:24:52 -0500

    commit

--- src/main/java/com/careerdevs/muzick1/controllers/AuthController.java
+++ src/main/java/com/careerdevs/muzick1/controllers/AuthController.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -50,6 +51,11 @@ public class AuthController {
 
     @Autowired
     private JwtUtils jwtUtils;
+
+    @GetMapping("/test")
+    public ResponseEntity<String> testRoute() {
+        return new ResponseEntity<>("Test", HttpStatus.OK);
+    }
 
     @PostMapping("/signin")
     public ResponseEntity<JwtResponse> authentication(@RequestBody LogInRequest logInRequest) {
